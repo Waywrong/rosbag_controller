1bab9a6685d4c5de732f61547ef0152737d2a2b3
Waywrong <wlhsu@ira.ee.ntu.edu.tw>
2017-05-04 20:24:27 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rosbag_controller
 A ros node that wraps rosbag and provides services to pragmatically pause and unpause the bag.
+
+
+roslaunch
+=======
+
+	roslaunch rosbag_controller rosbag_controller.launch
+
+rosservice
+=======
+	rosservice call /bag_go 
+	rosservice call /pause_bag 
--- src/rosbag_controller_node.cpp
+++ src/rosbag_controller_node.cpp
@@ -47,14 +47,15 @@ int main(int argc, char * argv[]) {
 
   // --- construct the argument vector and spawn rosbag
 
-  const char* rosbag_args[6];
+  const char* rosbag_args[7];
 
   rosbag_args[0] = argv[1];
   rosbag_args[1] = "play";
   rosbag_args[2] = "--clock";
   rosbag_args[3] = "--pause";
-  rosbag_args[4] = argv[2];
-  rosbag_args[5] = (const char *) NULL;
+  rosbag_args[4] = argv[3];//rate
+  rosbag_args[5] = argv[2];//bag_path
+  rosbag_args[6] = (const char *) NULL;
 
   rosbag = new Spawner(rosbag_args);
 
@@ -73,20 +74,20 @@ int main(int argc, char * argv[]) {
   // --- advertise pause and unpause services
 
   ros::ServiceServer pause_srv = n.advertiseService("pause_bag", pause);
-  ros::ServiceServer unpause_srv = n.advertiseService("unpause_bag", unpause);
+  ros::ServiceServer unpause_srv = n.advertiseService("bag_go", unpause);
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
   // --- consume output from rosbag;
 
-  char buf[1024];
+  char buf[10240];
 
   ros::Rate r(10);
 
   while (ros::ok()) {
     r.sleep();
-    rosbag->stdout.readsome(buf, 1024);
+    rosbag->stdout.readsome(buf, 10240);
   }
 
   ros::waitForShutdown();
